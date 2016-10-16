de4f59ffea1edbc595af1bdf868789f4f85d32a2
BedrockDev <dev.bedrock@gmail.com>
2016-10-16 21:20:01 +0900

    size comparison

---
+++ Programming/ConsoleApplication27/ConsoleApplication27/size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main() {
+	printf("Size of char   : %d\n", sizeof(char));
+	printf("Size of char[5]: %d\n", sizeof(char[5]));
+	printf("Size of int    : %d\n", sizeof(int));
+	printf("Size of short  : %d\n", sizeof(short));
+	printf("Size of long   : %d\n", sizeof(long));
+	printf("Size of float  : %d\n", sizeof(float));
+	printf("Size of double : %d\n", sizeof(double));
+}
