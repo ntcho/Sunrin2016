c637b81058a07c6b2acfd10a32fedf4cc7921278
BedrockDev <dev.bedrock@gmail.com>
2016-12-01 12:30:44 +0900

    duplicated same string length

--- Programming/ConsoleApplication33/ConsoleApplication33/test9.c
+++ Programming/ConsoleApplication33/ConsoleApplication33/test9.c
@@ -3,14 +3,15 @@
 void main() {
 	FILE *input = fopen("input9.txt", "r");
 	FILE *output = fopen("output9.txt", "w");
-	int i;
+	int i, j;
 	char string[100];
 	int stringLength = 0;
 	int wordLength = 0;
 	int wordMaxLength = 0;
-	int wordPosition = 0;
+	int wordPosition[100] = { 0 };
+	int wordPositionCounter = 0;
 
-	printf("Output (output9.txt)\n");
+	printf("Output (output9.txt)\n\n");
 	fgets(string, 100, input);
 
 	for (i = 0; i < 100; i++) {
@@ -18,7 +19,11 @@ void main() {
 			if (string[i] == 32) {
 				if (wordLength > wordMaxLength) {
 					wordMaxLength = wordLength;
-					wordPosition = i - wordMaxLength;
+					wordPositionCounter = 0;
+					wordPosition[wordPositionCounter] = i - wordMaxLength;
+				}
+				else if (wordLength == wordMaxLength) {
+					wordPosition[++wordPositionCounter] = i - wordMaxLength;
 				}
 				wordLength = 0;
 			}
@@ -32,20 +37,46 @@ void main() {
 		}
 	}
 
-	printf("Length of string : %d\n", stringLength);
-	printf("Starting position of the longest word : %d\n", wordPosition + 1);
-	printf("The longest word in string : \"");
-	for (i = 0; i < wordMaxLength; i++) {
-		printf("%c", string[wordPosition + i]);
+	printf("Length of full string : %d\n", stringLength);
+	printf("Starting position of the longest word(s) :");
+	for (i = 0; i < wordPositionCounter + 1; i++) {
+		if (i > 0) {
+			printf(",");
+		}
+		printf(" %d", wordPosition[i] + 1);
 	}
-	printf("\"\n"); 
-	
-	fprintf(output, "Length of string : %d\n", stringLength);
-	fprintf(output, "Starting position of the longest word : %d\n", wordPosition + 1);
-	fprintf(output, "The longest word in string : \"");
-	for (i = 0; i < wordMaxLength; i++) {
-		fprintf(output, "%c", string[wordPosition + i]);
+
+	printf("\nThe longest word in string : \"");
+	for (j = 0; j < wordPositionCounter + 1; j++) {
+		if (j > 0) {
+			printf("\", \"");
+		}
+		for (i = 0; i < wordMaxLength; i++) {
+			printf("%c", string[wordPosition[j] + i]);
+		}
 	}
+
+	printf("\"\n");
+
+	fprintf(output, "Length of full string : %d\n", stringLength);
+	fprintf(output, "Starting position of the longest word(s) :");
+	for (i = 0; i < wordPositionCounter + 1; i++) {
+		if (i > 0) {
+			fprintf(output, ",");
+		}
+		fprintf(output, " %d", wordPosition[i] + 1);
+	}
+
+	fprintf(output, "\nThe longest word in string : \"");
+	for (j = 0; j < wordPositionCounter + 1; j++) {
+		if (j > 0) {
+			fprintf(output, "\", \"");
+		}
+		for (i = 0; i < wordMaxLength; i++) {
+			fprintf(output, "%c", string[wordPosition[j] + i]);
+		}
+	}
+
 	fprintf(output, "\"\n");
 
 	fclose(input);
